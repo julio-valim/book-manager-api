bacac9dbaf3695a87a17886ec55a55eca6a43305
julio-valim <juliopvalim@gmail.com>
2023-08-24 20:51:50 -0300

    changing project build tool

--- src/main/java/com/bookstore/bookmanager/BookmanagerApplication.java
+++ src/main/java/com/bookstore/bookmanager/BookmanagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookManagerApplication {
+public class BookmanagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookManagerApplication.class, args);
+		SpringApplication.run(BookmanagerApplication.class, args);
 	}
 
 }
