398e1738b9fd27306967be357e594be8046f7d38
julio-valim <juliopvalim@gmail.com>
2023-08-25 19:00:06 -0300

    set cors configuration

---
+++ src/main/java/com/bookstore/bookmanager/config/web.java
@@ -0,0 +1,19 @@
+package com.bookstore.bookmanager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class web implements WebMvcConfigurer {
+
+  public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "PUT", "POST", "DELETE");
+    }
+  
+}
